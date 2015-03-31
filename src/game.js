c2c072529641da4f3ca4f341a98540e6ed4b0158
ZaruWright <samuel_gse@hotmail.com>
2015-03-31 16:50:53 +0200

    All ready to startgit status

---
+++ src/game.js
@@ -0,0 +1,11 @@
+ 
+var game = function(){
+
+var Q = Quintus()
+        .include("Sprites, Scenes, Input, 2D, Touch, UI")
+        .setup({ maximize: true })
+        .controls().touch()
+        
+
+
+};
