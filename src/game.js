00c43b3ddbcc6ceed61b4b914abbe21780f3dfa4
ZaruWright <samuel_gse@hotmail.com>
2015-04-02 16:10:33 +0200

    Perfect, I have the background

--- src/game.js
+++ src/game.js
@@ -2,34 +2,22 @@
 var game = function(){
 
 var Q = Quintus({development: true})
-        .include("Sprites, Scenes, Input, Touch, UI, Anim, TMX")
-        .setup("canvasGame")
+        .include("Scenes, Input, Sprites, UI, Touch, TMX, Anim, 2D")
+        .setup({width: 320, height:480})
         .controls().touch()
 
 
-
-
-// Create a scene
-Q.scene("level1", function(stage){
-  Q.stageTMX("mario.tmx", stage);
-  stage.add("viewport").centerOn(150,380);
-});
-
 // Load tmx level
-Q.loadTMX("mario.tmx, mario1.png, mario2.png", function(){
-  /*Q.sheet("tiles","tiles.png", {
-    tilew: 34,
-    tileh: 34,
-  });*/
-  Q.sheet("mario1","mario1.png", {
-    tilew: 32,
-    tileh: 32,
-  });
-  Q.sheet("mario2","mario2.png", {
-    tilew: 32,
-    tileh: 32,
-  });
+Q.loadTMX("level.tmx, tiles.png", function(){
   Q.stageScene("level1");
 });
 
-};
+// Create a scene
+Q.scene("level1", function(stage){
+  Q.stageTMX("level.tmx", stage);
+  stage.add("viewport").centerOn(150,380);
+});
+
+
+
+}; 
