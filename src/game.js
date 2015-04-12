8b58f89d3d2ea939c23feb7b1d0766f9fa54f918
ZaruWright <samuel_gse@hotmail.com>
2015-04-12 20:01:33 +0200

    it works the animations less the mario dead animation

--- src/game.js
+++ src/game.js
@@ -82,9 +82,7 @@ Q.scene("dialog", function(stage){
   box.fit(20);
 });
 
-/*Q.animations('marioAnimations', {
-  stand_left: {frames: }
-});*/
+
 
 //************
 // Mario
@@ -93,21 +91,71 @@ Q.Sprite.extend("Mario", {
   init: function(p){
     this._super(p,{
       sheet: "marioR",
+      sprite: "marioAnimations",
       x: 150,
-      y: 500,
+      y: 500
       
     });
 
-    this.add('2d, platformerControls');
+    this.add('2d, platformerControls, animation');
+    
+    Q.input.on("keydown", this, "move");
+    this.on("jump", this, "jump");
+    this.on("jumped", this, "jumped");
+    this.on("hit.sprite", this, "arrivesToPrincess");
+  },
 
-    this.on("hit.sprite", function(collision){
-      if (collision.obj.isA("Princess")){
-        Q.stageScene("dialog",1, { label: "You win", sceneToGo: "mainTitle"});
-        this.del('platformerControls');
-      }
-    });
+  move: function(key){
+    var keydown = Q.input.keys[key];
+    if (keydown == "left"){
+      this.play("walk_left");
+    }
+    else if (keydown == "right"){
+      this.play("walk_right"); 
+    }
+  },
+
+  jump: function(){
+    if (this.p.direction == "left"){
+      this.play("jump_left");
+    }
+    else if (this.p.direction == "right"){
+      this.play("jump_right");
+    }
+  },
+
+  jumped: function(){
+    if (this.p.direction == "left"){
+      this.play("stand_left");
+    }
+    else if (this.p.direction == "right"){
+      this.play("stand_right");
+    }
+  },
+
+  arrivesToPrincess: function(collision){
+    if (collision.obj.isA("Princess")){
+      Q.stageScene("dialog",1, { label: "You win", sceneToGo: "mainTitle"});
+      this.del('platformerControls');
+    }
+  },
+
+  step: function(dt){
+    if (this.p.y > 600){
+      Q.stageScene("mainTitle");
+    }
   }
 
+});
+
+Q.animations('marioAnimations', {
+  stand_right: {frames: [0], loop:false },
+  stand_left: {frames: [14], loop:false },
+  walk_right: {frames: [1,2,3], rate: 1/4, next: 'stand_right'},
+  walk_left: {frames: [15,16,17], rate: 1/4, next: 'stand_left'},
+  jump_right: {frames: [4], loop: false},
+  jump_left: {frames: [18], loop: false},
+  die: {frames: [13], loop:false}
 });
 
 //************
@@ -117,28 +165,55 @@ Q.Sprite.extend("Goomba", {
   init: function(p){
     this._super(p,{
       sheet: "goomba",
+      sprite: "goombaAnimations",
       x: 310,
       y: 500,
       vx: 80
     });
 
-    this.add('2d, aiBounce');
-    this.on("bump.left, bump.right", function(collision){
-      if (collision.obj.isA("Mario")){
+    this.add('2d, aiBounce, animation');
+
+    this.play("walk");
+
+    this.on("bump.left, bump.right", this, "hitPlayer");
+    this.on("bump.top", this, "die");
+    
+  },
+
+  hitPlayer: function(collision){
+    if (collision.obj.isA("Mario")){
         Q.stageScene("dialog",1, { label: "You Died", sceneToGo: "mainTitle"});
-        collision.obj.destroy();
+        collision.obj.play("die");
+        collision.obj.p.die = true;
+        //collision.obj.p.ay = 2;
+        //collision.obj.destroy();
       }
-    });
-    this.on("bump.top", function(collision){
-      if (collision.obj.isA("Mario")){
-        this.destroy();
-      }
-    });
+  },
+
+  die: function(collision){
+    if (collision.obj.isA("Mario")){
+      this.play("die");
+      this.p.die = true;
+      this.p.deadTimer = 0;
+    }
   },
 
   step: function(dt){
-
+    if (this.p.die){
+      this.p.deadTimer++;
+      this.del('2d, aiBounce');
+      if (this.p.deadTimer > 24){
+        this.destroy();
+      }
+    }
   }
+
+
+});
+
+Q.animations('goombaAnimations', {
+  walk: {frames: [0,1], rate: 1/2},
+  die: {frames: [2], loop:false}
 });
 
 //************
@@ -148,6 +223,7 @@ Q.Sprite.extend("Bloopa", {
   init: function(p){
     this._super(p,{
       sheet: "bloopa",
+      sprite:"bloopaAnimations",
       x: 330,
       y: 290,
       gravity: 0,
@@ -155,33 +231,58 @@ Q.Sprite.extend("Bloopa", {
       direction: "down"
     });
 
-    this.add('2d');
+    this.add('2d, animation');
 
-    this.on("bump.left, bump.right, bump.bottom", function(collision){
-      if (collision.obj.isA("Mario")){
-        Q.stageScene("dialog",1, { label: "You Died", sceneToGo: "mainTitle"});
-        collision.obj.destroy();
-        this.p.vy = this.p.vyAux;
-      }
-    });
-    this.on("bump.top", function(collision){
-      if (collision.obj.isA("Mario")){
-        this.destroy();
-      }
-    });
+    this.play("move");
+
+    this.on("bump.left, bump.right, bump.bottom", this, "hitPlayer");
+    this.on("bump.top", this, "die");
+  },
+
+  hitPlayer: function(collision){
+    if (collision.obj.isA("Mario")){
+      Q.stageScene("dialog",1, { label: "You Died", sceneToGo: "mainTitle"});
+      collision.obj.play("die");
+      collision.obj.p.die = true;
+      collision.obj.p.ay = 2;
+      //collision.obj.destroy();
+      this.p.vy = this.p.vyAux;
+    }
+  },
+
+  die: function(collision){
+    if (collision.obj.isA("Mario")){
+      this.play("die");
+      this.p.die = true;
+      this.p.deadTimer = 0;
+    }
   },
 
   step: function(dt){
+
+    if (this.p.die){
+      this.p.deadTimer++;
+      this.del('2d');
+      if (this.p.deadTimer > 24){
+        this.destroy();
+      }
+    }
+
     if (this.p.y >= 500 && this.p.direction == "down"){
       this.p.vy = -this.p.vy;
       this.p.direction = "up";
     }
-    else if (this.p.y <= 300 && this.p.direction == "up"){
+    else if (this.p.y <= 300 && this.p.direction == "up" && !this.p.die){
       this.p.vy = -this.p.vy;
       this.p.direction = "down";
     }
     this.p.vyAux = this.p.vy;
   }
+});
+
+Q.animations('bloopaAnimations', {
+  move: {frames: [0,1], rate: 1/2},
+  die: {frames: [2], loop:false}
 });
 
 //************
